ac1e7bdbbf47afeaa1c49c12d2607d29c66b64ba
slivers <slivers@ukr.net>
2016-03-18 19:49:50 +0200

    test task

--- test/client/main.js
+++ test/client/main.js
@@ -3,3 +3,18 @@ var todos = [
   'Write code',
   'Pass test'
 ];
+var list = document.getElementById("app");
+var arrLength = todos.length;
+for(var i=0; i<arrLength; i++){
+  var li = document.createElement('li');
+  li.innerHTML = todos[i];
+  list.appendChild(li);
+}
+
+list.addEventListener("click" , function(event) {
+  var el = event.target;
+  if(el.style.textDecoration==='line-through')
+    el.style.textDecoration = 'none';
+  else
+    el.style.textDecoration = 'line-through';
+});
