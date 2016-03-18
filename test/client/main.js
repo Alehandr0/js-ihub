298d844ea2472bf347f48e845c694ff89d1c1cbc
Dima Sh <shimdim@gmail.com>
2016-03-19 00:30:16 +0200

    client

--- test/client/main.js
+++ test/client/main.js
@@ -3,3 +3,16 @@ var todos = [
   'Write code',
   'Pass test'
 ];
+var app = document.getElementById('app');
+function createTodo(str){
+    var todoElem = document.createElement('li');
+    todoElem.innerHTML = str;
+    app.appendChild(todoElem);
+}
+for (var i = 0; i < todos.length; i++) {
+    createTodo(todos[i]);
+}
+app.onclick = function(e){
+    var target = e.target;
+    target.classList.toggle('done');
+}
