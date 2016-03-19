cf22342a946e9c205d15a2bebb30ae07977db9d2
alex-arkhypenko <alex.arkhypenko@gmail.com>
2016-03-19 13:40:40 +0200

    calculator.js
    done

--- test/node/calculator.js
+++ test/node/calculator.js
@@ -1,3 +1,21 @@
 module.exports = function calculator(expr) {
-  return eval(expr);
+
+	try{
+		if ((typeof expr) !== "string" ) {
+
+			console.log('TypeError: Expression should be a string.');
+
+		}
+		else{
+
+		  	return eval(expr);
+
+		}; 
+
+	}
+	catch(err){
+
+		console.log('Error: "' + expr + '" is not valid expression.');	
+		
+	} 	
 }
