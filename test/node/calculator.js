2239a0078f0212cecd3503c4a3e54cae5c9c5b58
slivers <slivers@ukr.net>
2016-03-19 19:55:30 +0200

    node task

--- test/node/calculator.js
+++ test/node/calculator.js
@@ -1,3 +1,15 @@
 module.exports = function calculator(expr) {
-  return eval(expr);
-}
+    if (typeof expr === 'string'){
+        try{
+            var res = eval(expr);
+        }
+        catch(e){
+            res = 'Error: ' + expr +' is not valid expression.';
+        }
+        finally {
+            return res;
+        }
+    }
+    else
+        return 'TypeError: Expression should be a string.';
+ }
