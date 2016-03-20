85c0fd76282b1d8ea94d29aef571b1597e77890a
silika <s.svyrydenko@gmail.com>
2016-03-20 20:04:54 +0200

    Test tasks were completed

--- test/client/main.js
+++ test/client/main.js
@@ -1,5 +1,32 @@
-var todos = [
+(function (){
+  var todos = [
   'Get test exercise',
   'Write code',
   'Pass test'
 ];
+  // render todoItems in todosList
+  todos.forEach(function(todo){
+
+    var todoListItem = document.createElement('li'),
+        node = document.createTextNode(todo);
+
+    todoListItem.appendChild(node);
+    app.appendChild(todoListItem);
+
+  });
+
+  //change todoItem state
+  app.onclick = todoChangeState;
+
+  function todoChangeState(event){
+    var element = event.target;
+    if (element.tagName == 'LI') {
+      if (element.style.textDecoration.length) {
+        element.style.textDecoration = ''
+      } else {
+        element.style.textDecoration = 'line-through';
+      }
+    }
+  }
+
+})();
--- test/node/index.js
+++ test/node/index.js
@@ -1,6 +1,41 @@
 
-console.log(calculator('40 + 2')); // 42
+    var makeCalculation = require('./calculator');
 
-console.log(calculator(123)); // TypeError: Expression should be a string.
+    function calculator(expression) {
 
-console.log(calculator('40 + hello')); // Error: "40 + hello" is not valid expression.
+        var isValidExpression = expressionValidator(expression),
+            isValidTypeExpression = expressionTypeValidator(expression),
+            typeErrorMsg = 'TypeError: Expression should be a string.',
+            errorMsg = 'Error: "' + expression +'" is not valid expression.';
+
+        if (isValidTypeExpression && isValidExpression) {
+            // additional check, in case something is wrong
+            try {
+                return makeCalculation(expression);
+            }
+            catch(err) {
+                return errorMsg
+            }
+        }
+        if (!isValidTypeExpression) return typeErrorMsg;
+        if (!isValidExpression) return errorMsg;
+
+    }
+
+    function expressionValidator(expression){
+        var template = new RegExp("^[\\d\\+\\/\\*\\.\\- \\(\\)]*$");
+        return template.test(expression);
+    }
+
+    function expressionTypeValidator(expression) {
+        return typeof expression == 'string'
+    }
+
+    console.log(calculator('40 + 2')); // 42
+
+    console.log(calculator(123)); // TypeError: Expression should be a string.
+
+    console.log(calculator('40 + hello')); // Error: "40 + hello" is not valid expression.
+
+
+
