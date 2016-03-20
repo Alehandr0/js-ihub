20d0605bb97e41791ccba14f9e2c9b55463e7464
slastonm <slastonm@gmail.com>
2016-03-20 23:17:04 +0200

    Todo creating by student slastonm

--- test/client/main.js
+++ test/client/main.js
@@ -3,3 +3,35 @@ var todos = [
   'Write code',
   'Pass test'
 ];
+
+
+"use strict";
+
+(function () {
+	var listBlock = document.getElementById("app");
+	var listItem = document.createElement("li");
+	todos.forEach(function(item, i, arr) {		
+		var listItem = document.createElement("li");
+		listItem.innerHTML=item;
+		listBlock.appendChild(listItem);
+	});
+
+
+
+	listBlock.addEventListener('click', function(e) {
+	    if (e.target.tagName === 'LI'){
+	    	if(e.target.className == "done"){
+				e.target.className = " ";
+	    	}
+	    	else{
+	    		e.target.className = "done";
+	    	}
+	    	
+	     
+	    }
+	});
+
+	
+})();
+
+
