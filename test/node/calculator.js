74dc55ddb4d6ecc409d4d7429bc98a2df61eb0d9
Bogdan <bs08@ukr.net>
2016-03-20 16:28:06 +0200

    Added files via upload

--- test/node/calculator.js
+++ test/node/calculator.js
@@ -1,3 +1,14 @@
-module.exports = function calculator(expr) {
-  return eval(expr);
-}
+module.exports = function calculator(expr) { 
+  answer = undefined;
+  if ('string' != typeof expr) { 
+  	answer = 'TypeError: Expression should be a string.'
+  } else { 
+  		try { 
+  			answer = eval(expr) 
+  		} catch(er) { 
+  			answer = 'Error: "' + expr + '" is not valid expression.' 
+  		}
+  	};
+
+  return answer; 
+} 
--- test/node/index.js
+++ test/node/index.js
@@ -1,6 +1,6 @@
-
-console.log(calculator('40 + 2')); // 42
-
-console.log(calculator(123)); // TypeError: Expression should be a string.
-
+var calculator = require('./calculator');
+console.log(calculator('40 + 2')); // 42 
+ 
+console.log(calculator(123)); // TypeError: Expression should be a string. 
+ 
 console.log(calculator('40 + hello')); // Error: "40 + hello" is not valid expression.
