4a384d96d4da948cce24deb71c319c3667b9ce2e
Alex <Alehandr0@users.noreply.github.com>
2016-03-21 21:30:20 +0200

    Update main.js

--- test/client/main.js
+++ test/client/main.js
@@ -3,3 +3,28 @@ var todos = [
   'Write code',
   'Pass test'
 ];
+function start()
+  {
+	var x="";
+	for(i=0; i<=todos.length; x+="<li id="+i+" onclick='per("+i+")'>"+todos[i]+"</li>", i++)
+	document.getElementById("app").innerHTML=x;
+}
+function per(n)
+  {
+	var	p=document.getElementById(n).innerHTML,
+		f1="document.getElementById(n).style.textDecoration=",
+		f2="document.getElementById(n).innerHTML=todos[n]";
+	if(p==todos[n])
+	  {
+		f1+="'line-through'";
+		eval(f1);
+		f2+="+' '";
+		eval(f2);
+	  }
+	  else
+	  {
+		f1+="'none'";
+		eval(f1);
+		eval(f2);
+	}
+}
