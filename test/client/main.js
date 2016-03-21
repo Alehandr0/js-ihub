c78c18ab327244648d27959ad2eca691eae719f8
AV1611 <alexej.ashkenazi@gmail.com>
2016-03-21 14:17:28 +0200

    Commiting, main.js and node.js changed.

--- test/client/main.js
+++ test/client/main.js
@@ -1,5 +1,31 @@
-var todos = [
+(function () {
+  var toDoList = [
   'Get test exercise',
   'Write code',
   'Pass test'
 ];
+
+  toDoList.forEach(function(agenda) {
+
+    var todoListItem = document.createElement('li'),
+        node = document.createTextNode(agenda);
+
+    todoListItem.appendChild(node);
+    app.appendChild(todoListItem);
+
+  });
+
+  app.onclick = todoChangeState;
+
+  function todoChangeState(event) {
+    var element = event.target;
+    if (element.tagName == 'LI') {
+      if (element.style.textDecoration.length) {
+        element.style.textDecoration = ''
+      } 
+      else {
+        element.style.textDecoration = 'line-through';
+      }
+    }
+  }
+}) ();
