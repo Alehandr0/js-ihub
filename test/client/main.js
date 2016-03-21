51bd41b338e00f3ef109e9fedc398341520e2208
turbowind <turbowind.dzen@yandex.ua>
2016-03-21 18:46:06 +0200

    extend client part

--- test/client/main.js
+++ test/client/main.js
@@ -3,3 +3,16 @@ var todos = [
   'Write code',
   'Pass test'
 ];
+
+var app = document.getElementById('app');
+
+todos.forEach(function(todo) {
+  var elem = document.createElement('li');
+  elem.innerHTML = todo;
+  app.appendChild(elem);
+});
+
+app.onclick = function(e) {
+  var target = e.target;
+  target.classList.toggle('clicked');
+};
