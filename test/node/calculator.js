236a5b94b873ab0b283a6865d2a71625acafac88
Alex <Alehandr0@users.noreply.github.com>
2016-03-22 21:03:42 +0200

    Update calculator.js

--- test/node/calculator.js
+++ test/node/calculator.js
@@ -1,3 +1,13 @@
-module.exports = function calculator(expr) {
-  return eval(expr);
+module.exports = function calculator(expr)
+  {
+    if (typeof(expr) != "string") return 'TypeError: Expression should be a string.';
+    else
+    try
+	    {
+        return eval(expr);
+	    }
+      catch(error)
+	    {
+      return 'Error: "'+expr+'" is not valid expression.';
+    }
 }
