d006fc407443fc11da9de4a8985dc155b0dc9025
Alex <Alehandr0@users.noreply.github.com>
2016-03-22 20:52:33 +0200

    Update index.js

--- test/node/index.js
+++ test/node/index.js
@@ -1,6 +1,7 @@
+var calculator=require("./calculator.js");
 
-console.log(calculator('40 + 2')); // 42
+console.log(calculator('40 + 2'));			// 42
 
-console.log(calculator(123)); // TypeError: Expression should be a string.
+console.log(calculator(123));				// TypeError: Expression should be a string.
 
-console.log(calculator('40 + hello')); // Error: "40 + hello" is not valid expression.
+console.log(calculator('40 + hello'));		// Error: "40 + hello" is not valid expression.
